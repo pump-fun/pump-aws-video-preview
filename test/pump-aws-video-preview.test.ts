61eda8c992ddd1a385f99cfcde9a5f75934236ae
Arvind Krishnsn <arvind@Arvinds-MacBook-Pro.local>
2024-10-13 14:22:11 +0530

    Initial commit

---
+++ test/pump-aws-video-preview.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PumpAwsVideoPreview from '../lib/pump-aws-video-preview-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pump-aws-video-preview-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PumpAwsVideoPreview.PumpAwsVideoPreviewStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
