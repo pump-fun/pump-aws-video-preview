6b8175dbc6bfc057404d7db57817ee126a360161
Arvind Krishnsn <arvind@Arvinds-MacBook-Pro.local>
2024-10-16 18:56:16 +0530

    pipeline to bin

--- bin/pump-aws-video-preview.ts
+++ bin/pump-aws-video-preview.ts
@@ -5,13 +5,13 @@ import { VideoPreviewStack } from '../lib/pump-media/stack/video-preview-stack';
 import { PumpAwsVideoCdkPipelineStack } from '../lib/pipeline/pump-aws-video-cdk-pipeline-stack';
 
 const app = new cdk.App();
-new VideoPreviewStack(app, "Local-video-preview", {
-  env: { account: "026090514015", region: "us-east-1" },
-  environment: "Local"
-})
-
-// new PumpAwsVideoCdkPipelineStack(app, "Video-Pipeline", {
-//   env: { account: "026090514274", region: "eu-west-1" },
+// new VideoPreviewStack(app, "Local-video-preview", {
+//   env: { account: "026090514015", region: "us-east-1" },
+//   environment: "Local"
 // })
+
+new PumpAwsVideoCdkPipelineStack(app, "Video-Pipeline", {
+  env: { account: "026090514274", region: "eu-west-1" },
+})
 
 app.synth();
